b2cb6ba10fe56436609f27a2ed6de4a6efe752ce
Smit Khandelwal <smitkhandelwal@gmail.com>
2024-06-07 21:39:27 +0530

    initial commit

---
+++ backend/Server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const routes = require("./routes/ToDoRoutes");
+
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(`mongodb+srv://smit:ipass@cluster0.wtjn8.mongodb.net/todo`)
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+app.use("/api", routes);
+
+app.listen(PORT, () => console.log(`Listening at ${PORT}...`));
